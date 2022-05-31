a119385636b076f83fd774ed6eb52c8044d76c6f
DoanHau <>
2022-05-31 07:01:35 +0000

    Add todo

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
-/* 
-  【inputコンポーネント】
-　・新しいTodoを作成するINPUTフィールドを作成するコンポーネント
-　・Enterをクリックされたら入力された文字を使って新しいTodoを作成する
-*/
-function Input( {onAdd} ) {
-  const handleChange = e => setText(e.target.value);
+
+function Input( { onAdd } ) {
   const [text, setText] = React.useState('');
-  const handleEnter = e => {
+
+  const handleChange = e => setText(e.target.value);
+
+  const handleKeyPress = e => {
     if (e.keyCode === 13) {
-      onAdd(text);//add to list
-      setText('');//reset
+      onAdd(text);
+      setText('');
     }
   };
+
   return (
     <div className="panel-block">
       <input
@@ -21,7 +20,7 @@ function Input( {onAdd} ) {
         placeholder="Todoを入力してください"
         value={text}
         onChange={handleChange}
-        onEnter={handleEnter}
+        onKeyDown={handleKeyPress}
       />
     </div>
   );
